2e589773fd60c698996884e8bd6af2979ad3d6cd
Ayodele Opetumo <ayodele.opetumo@andela.com>
2021-03-21 14:57:34 +0100

    Initial commit and cleanup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Star Wars React App</h1>
+    </div>
+  );
+}
+
+export default App;
